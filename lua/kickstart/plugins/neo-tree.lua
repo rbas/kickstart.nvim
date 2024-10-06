600bc041a19419018e0ba53dcb171276a351a127
Martin Voldřich <rbas.cz@gmail.com>
2024-10-06 11:32:49 +0200

    Definging visibility in neo tree

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -20,6 +20,12 @@ return {
           ['\\'] = 'close_window',
         },
       },
+      filtered_items = {
+        visible = true,
+        hide_by_name = {
+          '.DS_Store',
+        },
+      },
     },
   },
 }
