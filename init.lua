ef1e2a0b6bc0a6f19cca5890132a37edb90a0041
Martin Voldřich <rbas.cz@gmail.com>
2024-10-06 11:33:28 +0200

    Added lezygit and a lsp configruation

--- init.lua
+++ init.lua
@@ -569,14 +569,18 @@ require('lazy').setup({
       --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
+      --local util = require "lspconfig.util"
       local servers = {
         -- clangd = {},
-        -- gopls = {},
+        gopls = {},
         pyright = {},
         rust_analyzer = {
           settings = {
             ['rust-analyzer'] = {
               cargo = { allFeatures = true, features = { 'all' } },
+              checkOnSave = {
+                command = 'clippy',
+              },
             },
           },
         },
@@ -908,7 +912,7 @@ require('lazy').setup({
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
